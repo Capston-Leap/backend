4f006dbd9b1f5933a3bb95dd9fe66f2836ca39d9
SeolA-Lee <dlcodus1201@naver.com>
2025-04-12 23:56:40 +0900

    refactor: ApiResponse 에러 사항 추가(400)

--- src/main/java/com/dash/leap/domain/information/controller/docs/InformationControllerDocs.java
+++ src/main/java/com/dash/leap/domain/information/controller/docs/InformationControllerDocs.java
@@ -5,6 +5,7 @@ import com.dash.leap.domain.information.entity.enums.InfoType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface InformationControllerDocs {
 
     @Operation(summary = "자립지원정보 조회", description = "자립지원정보 목록을 조회합니다.")
-    @ApiResponse(description = "자립지원정보 목록 조회 성공", responseCode = "200")
+    @ApiResponses(value = {
+            @ApiResponse(description = "자립지원정보 목록 조회 성공", responseCode = "200"),
+            @ApiResponse(description = "자립지원정보 목록 조회 실패(잘못된 파라미터 값 입력)", responseCode = "400")
+    })
     ResponseEntity<InformationListResponse> readAllInformation(
             @Parameter(description = "자립지원정보 카테고리(미입력 시 전체 조회)")
             @RequestParam(name = "category", required = false) InfoType infoType,
