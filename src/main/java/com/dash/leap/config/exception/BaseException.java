33be3bd01b8718f546bd1747c688d299ae8f23da
SeolA-Lee <dlcodus1201@naver.com>
2025-04-13 23:42:03 +0900

    feat: BaseException 추가

---
+++ src/main/java/com/dash/leap/config/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.dash.leap.config.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public BaseException(final HttpStatus status, final String message) {
+        super(message);
+        this.status = status;
+    }
+}
