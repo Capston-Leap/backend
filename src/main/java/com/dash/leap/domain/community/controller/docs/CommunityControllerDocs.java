2b1d1689f5899fc93b04e46d1fde1fbfcc6e83c7
dayeee5 <dayeee5@naver.com>
2025-04-18 00:36:29 +0900

    refactor: 커뮤니티 Swagger 문서 수정

--- src/main/java/com/dash/leap/domain/community/controller/docs/CommunityControllerDocs.java
+++ src/main/java/com/dash/leap/domain/community/controller/docs/CommunityControllerDocs.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Community", description = "Community API")
 public interface CommunityControllerDocs {
@@ -17,9 +17,8 @@ public interface CommunityControllerDocs {
     // 커뮤니티 게시글 생성
     @Operation(summary = "게시글 생성", description = "커뮤니티에 게시글을 작성합니다.")
     @ApiResponse(responseCode = "201", description = "게시글 생성 성공")
-    @PostMapping("/{communityId}/post")
     ResponseEntity<PostCreateResponse> createPost(
-            @AuthenticationPrincipal Long userId,
+            Long userId,
             @PathVariable(name = "communityId") Long communityId,
             @RequestBody PostCreateRequest request
     );
@@ -27,9 +26,8 @@ public interface CommunityControllerDocs {
     // 커뮤니티 게시글 수정
     @Operation(summary = "게시글 수정", description = "커뮤니티의 특정 게시글을 수정합니다.")
     @ApiResponse(responseCode = "200", description = "게시글 수정 성공")
-    @PatchMapping("/{communityId}/post/{postId}")
     ResponseEntity<PostUpdateResponse> updatePost(
-            @AuthenticationPrincipal Long userId,
+            Long userId,
             @PathVariable(name = "communityId") Long communityId,
             @PathVariable(name = "postId") Long postId,
             @RequestBody PostUpdateRequest request
@@ -38,10 +36,9 @@ public interface CommunityControllerDocs {
     // 커뮤니티 게시글 삭제
     @Operation(summary = "게시글 삭제", description = "커뮤니티의 특정 게시글을 삭제합니다.")
     @ApiResponse(responseCode = "204", description = "게시글 삭제 성공")
-    @DeleteMapping("/{communityId}/post/{postId}")
     ResponseEntity<Void> deletePost(
             @PathVariable(name = "communityId") Long communityId,
             @PathVariable(name = "postId") Long postId,
-            @AuthenticationPrincipal Long userId
+            Long userId
     );
 }
