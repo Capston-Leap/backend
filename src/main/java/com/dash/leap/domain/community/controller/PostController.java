78713bff0d03e6d4eb03957eda02058e2b9000f7
dayeee5 <dayeee5@naver.com>
2025-04-17 18:25:59 +0900

    feat: 커뮤니티 게시글 수정

--- src/main/java/com/dash/leap/domain/community/controller/PostController.java
+++ src/main/java/com/dash/leap/domain/community/controller/PostController.java
@@ -2,10 +2,13 @@ package com.dash.leap.domain.community.controller;
 
 import com.dash.leap.domain.community.dto.request.PostCreateRequest;
 import com.dash.leap.domain.community.dto.response.PostCreateResponse;
+import com.dash.leap.domain.community.dto.request.PostUpdateRequest;
+import com.dash.leap.domain.community.dto.response.PostUpdateResponse;
 import com.dash.leap.domain.community.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +23,18 @@ public class PostController {
             @RequestBody PostCreateRequest request
     ) {
         return ResponseEntity.ok(postService.create(communityId, request));
+    }
+
+    // 커뮤니티 게시글 수정
+    @PatchMapping("/{communityId}/post/{postId}")
+    public ResponseEntity<PostUpdateResponse> updatePost(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable(name = "communityId") Long communityId,
+            @PathVariable(name = "postId") Long postId,
+            @RequestBody PostUpdateRequest request
+    ) {
+        return ResponseEntity.ok(
+                postService.update(postId, request, userId, communityId)
+        );
     }
 }
