1f5674e9b6a3385fc44e803580330c0a94520c97
SeolA-Lee <dlcodus1201@naver.com>
2025-04-20 23:48:19 +0900

    chore: Spring Security 기본 로그인폼 비활성화

--- src/main/java/com/dash/leap/global/config/security/SecurityConfig.java
+++ src/main/java/com/dash/leap/global/config/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.dash.leap.global.config.security;
 import com.dash.leap.global.auth.jwt.service.JwtAuthenticationFilter;
 import com.dash.leap.global.auth.jwt.service.JwtTokenProvider;
 import com.dash.leap.global.auth.user.CustomUserService;
+import com.dash.leap.global.exception.ExceptionResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +17,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,6 +29,7 @@ public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomUserService customUserService;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,10 +42,25 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, customUserService), UsernamePasswordAuthenticationFilter.class)
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .formLogin(AbstractAuthenticationFilterConfigurer::disable)
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(getAuthenticationEntryPoint()))
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint getAuthenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            ExceptionResponse body = new ExceptionResponse(HttpStatus.UNAUTHORIZED.toString(), "로그인이 필요합니다.");
+            String json = objectMapper.writeValueAsString(body);
+            response.getWriter().write(json);
+        };
     }
 
     @Bean
