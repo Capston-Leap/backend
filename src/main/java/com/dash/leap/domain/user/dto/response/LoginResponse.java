700f7a8177d222ce0ff5c35b4d002ea24fc4c8c7
SeolA-Lee <dlcodus1201@naver.com>
2025-04-24 19:06:55 +0900

    feat: LoginResponse에 userId 추가

--- src/main/java/com/dash/leap/domain/user/dto/response/LoginResponse.java
+++ src/main/java/com/dash/leap/domain/user/dto/response/LoginResponse.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "로그인 응답입니다.")
 public record LoginResponse(
-        @Schema(example = "{tokenString}")
+
+        @Schema(description = "회원 고유 ID", example = "1")
+        Long id,
+
+        @Schema(description = "JWT 토큰", example = "{tokenString}")
         String token
 ) {
 }
