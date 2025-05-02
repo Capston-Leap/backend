43df266ecc736b0cf8b9afa408070fc41b540852
dayeee5 <dayeee5@naver.com>
2025-05-02 17:18:49 +0900

    refactor: 감정일기 예외 처리 보완

---
+++ src/main/java/com/dash/leap/domain/diary/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.dash.leap.domain.diary.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
