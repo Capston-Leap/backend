a632516e1bfd446b35cbfe6f20b1c3550821d3ad
dayeee5 <dayeee5@naver.com>
2025-05-02 18:01:26 +0900

    refactor: swagger 예외 응답에 example 추가

--- src/main/java/com/dash/leap/global/exception/ExceptionResponse.java
+++ src/main/java/com/dash/leap/global/exception/ExceptionResponse.java
@@ -2,9 +2,8 @@ package com.dash.leap.global.exception;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "예외에 대한 응답 형식입니다.")
+@Schema(description = "예외 응답")
 public record ExceptionResponse(
-        String status,
-        String message
-) {
-}
+        @Schema(description = "HTTP 상태 코드", example = "상태 코드") String status,
+        @Schema(description = "에러 메시지", example = "에러 메시지") String message
+) {}
